321f0686cad764a59b714909a64a1013c827736d
welmomazepas <welmomazepas@gmail.com>
2025-02-26 14:28:57 -0300

    renamed DB on flask

--- flask_version/app.py
+++ flask_version/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db1.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
